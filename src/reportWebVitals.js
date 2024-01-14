4afa94ad8079205a179753b4f43d64612d619e08
Vishal suthar <117064555+Vishalsuthar88@users.noreply.github.com>
2024-01-14 20:29:49 +0530

    Delete src/reportWebVitals.js

--- src/reportWebVitals.js
+++
@@ -1,6 +0,0 @@
-// reportWebVitals.js
-export default function reportWebVitals(metric) {
-    console.log(metric);
-    // You can send the metric data to an analytics service or perform other actions here
-  }
-  
